ffdf673ad78ae1408377e70db89c02ae34f7cb0b
AN_PC <Genesis_AN@outlook.com>
2023-02-18 17:15:20 +0800

    add FromJson,TypeInt

--- Base/Card.go
+++ Base/Card.go
@@ -22,10 +22,36 @@ type ChaFileParameterEx struct {
 	Sex       int    `json:"sex"`
 }
 
+// TypeInt 将Card结构体中的CardType转换为int类型的函数
+func (c *Card) TypeInt() int {
+	switch c.CardType {
+	case CT_KK:
+		return CTI_Koikatu
+	case CT_KKP:
+		return CTI_KoikatsuParty
+	case CT_KKCSP:
+		return CTI_KoikatsuPartySpecialPatch
+	case CT_EC:
+		return CTI_EmotionCreators
+	case CT_AI:
+		return CTI_AiSyoujyo
+	case CT_KKS:
+		return CTI_KoikatsuSunshine
+	default:
+		return CTI_Unknown
+	}
+}
+
+// ToJson 将Card结构体转换为json字符串的函数
 func (c *Card) ToJson() (string, error) {
 	data, err := json.Marshal(c)
 	if err != nil {
 		return "", err
 	}
 	return string(data), nil
+}
+
+// FromJSON 将json字符串转换为Card结构体的函数
+func (c *Card) FromJSON(data string) error {
+	return json.Unmarshal([]byte(data), c)
 }
